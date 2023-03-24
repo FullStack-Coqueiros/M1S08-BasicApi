c3247c4b92e13299b49435ceb5dcccd3a1d5f65d
Vitor Lassen <vitor@lassen.com.br>
2023-03-24 19:47:35 -0300

    Lim,par a classes

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -7,14 +7,7 @@ namespace Basic_Api.Controllers
     [Route("clientes")]
     public class ClientesControllers : Controller
     {
-
         static List<Cliente> _clientes = new List<Cliente>();
-
-        //Métodod antigo        
-        // [HttpGet]
-        // public ActionResult Metodo ([FromQuery]string nome) { 
-        //     return Ok("Olá, "+ nome);
-        // }
         
         [HttpGet]
         public ActionResult Metodo ([FromQuery]string nome) { 
@@ -25,34 +18,18 @@ namespace Basic_Api.Controllers
             return Ok(filtroclientes);
         }
 
-        [HttpGet]
-        [Route("{id}")]
-        public string ObterClienteComId ([FromQuery]string nome, [FromRoute]int id){
-           return  "Olá, "+ nome+ " O Seu id é "+ id;
-        }
-        [HttpGet]
-        [Route("id/{id}")]
-         public ActionResult Metodo3 ([FromQuery]string nome, [FromRoute]int id){
-            return Ok(new Cliente(id, nome));
-         }
-
         [HttpPost]
         public ActionResult Post ([FromBody]Cliente cliente){
             _clientes.Add(cliente);
             return Created(Request.Path,cliente);
-
-
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public ActionResult Delete ([FromRoute]int id){
-            
+        public ActionResult Delete ([FromRoute]int id){  
             var cliente = _clientes.Find(x => x.Id == id);
             _clientes.Remove(cliente);
             return Ok();
-
-
         }
     }
 }
